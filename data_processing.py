44892e399c85751556203ece83c83b5b46d48503
SHUBHAM150802 <shubham9780907062@gmail.com>
2025-07-19 01:16:35 +0530

    Revert "added a new function sum"
    This reverts commit 26be508c3346159cd7d0ff0a5af2c5ee1f501b3c.
    
    git log

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,1 @@
 print("adding data processing in git bash")
-
-print("Adding new code for data processing...")
-
-a = 5
-b = 10
-
-def sum(a, b):
-    return a + b
-
-print("The sum of", a, "and", b, "is:", sum(a, b))
