abbe585918fb68c8cc56a8990f1a9e2c01793481
SHUBHAM150802 <shubham9780907062@gmail.com>
2025-07-19 22:12:17 +0530

    adding new functions in the code

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,19 @@
-print("adding data processing in git bash")
+print("adding data processing in git bash") 
+
+a = 5
+b = 10
+
+print("sum function")
+def sum(a, b):
+    c = a + b
+    return c
+
+print("multiplication function")
+def multiplication(a, b):
+    c = a * b   
+    return c
+
+print("subtraction function")
+def subtraction(a, b):
+    c = a - b
+    return c
